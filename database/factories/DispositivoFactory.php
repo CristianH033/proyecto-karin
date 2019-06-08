b36e279dd0b4046c1eed5e8991e98113f4c43201
Cristian David Home <cristian_david033@hotmail.com>
2019-06-08 17:11:28 -0500

    Creación de Modelos, Contorladores y Migraciones: -Persona -Pais -Estado -Ciudad -Sucursal -Proveedor -Cliente -Cargo -Funcionario -Dispositivo -Etiqueta -Reporte -Contrato

---
+++ database/factories/DispositivoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Dispositivo;
+use Faker\Generator as Faker;
+
+$factory->define(Dispositivo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SucursalFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Sucursal;
+use Faker\Generator as Faker;
+
+$factory->define(Sucursal::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
