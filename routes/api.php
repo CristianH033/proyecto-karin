9ae3124738af7e5d145603717fbfc19ee7ca106a
Cristian David Home <cristian_david033@hotmail.com>
2019-06-08 19:51:26 -0500

    Creación de rutas Route::resources

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * @Author: Cristian Home
+ * @Date: 2019-06-08 19:49:27
+ * @Desc: Creación dínamica de rutas para acceder a los recursos a través de una API.
+ */
+Route::resources([
+    'personas' => 'PersonaController',
+    'paises' => 'PaisController',
+    'estados' => 'EstadoController',
+    'ciudades' => 'CiudadController',
+    'sucursales' => 'SucursalController',
+    'proveedores' => 'ProveedorController',
+    'clientes' => 'ClienteController',
+    'cargos' => 'CargoController',
+    'funcionarios' => 'FuncionarioController',
+    'dispositivos' => 'DispositivoController',
+    'etiquetas' => 'EtiquetaController',
+    'reportes' => 'ReporteController',
+    'contratos' => 'ContratoController'
+]);
