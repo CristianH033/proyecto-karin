4e97c48ae016596a3b75a52afd161d438124d244
Cristian David Home <cristian_david033@hotmail.com>
2019-06-09 02:51:28 -0500

    Establecer ruta de redirección (Auth Controllers).

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -21,13 +21,6 @@ class ResetPasswordController extends Controller
     use ResetsPasswords;
 
     /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
      * Create a new controller instance.
      *
      * @return void
@@ -35,5 +28,14 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Funcioón Redirigir a
+     *
+     * @return String
+     */
+    function redirectTo(){
+        return route('home');
     }
 }
