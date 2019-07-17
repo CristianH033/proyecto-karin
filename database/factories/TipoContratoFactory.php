f9d62b4df7258f0f89cb8705d4d95d395990c43a
Cristian David Home <cristian_david033@hotmail.com>
2019-07-16 22:39:07 -0500

    Crear modelo "Tipo de Contrato"

---
+++ database/factories/TipoContratoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\TipoContrato;
+use Faker\Generator as Faker;
+
+$factory->define(TipoContrato::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
