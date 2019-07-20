1fd6aa987e3ec702b49d2aecbeac42ae39d4debc
Cristian David Home <cristian_david033@hotmail.com>
2019-07-20 00:18:08 -0500

    Importar componentes de manera dinámica (router).

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -5,106 +5,104 @@ import store from '@js/store'
 // Importar componentes de páginas
 
 // Componenes de DashBoard
-import DashBoardLayout from '@js/layouts/DashBoardLayout.vue';
-import Home from '@js/pages/Home.vue';
-import Contact from '@js/pages/Contact.vue';
-import About from '@js/pages/About.vue';
+const DashBoardLayout = () => import(/* webpackChunkName: "DashBoardLayout" */ '@js/layouts/DashBoardLayout.vue');
+const Home = () => import(/* webpackChunkName: "Home" */ '@js/pages/Home.vue');
+const Contact = () => import(/* webpackChunkName: "Contact" */ '@js/pages/Contact.vue');
+const About = () => import(/* webpackChunkName: "About" */ '@js/pages/About.vue');
+
 // Componenes de Autenticacion
-import AuthLayout from '@js/layouts/AuthLayout.vue';
-import Login from '@js/pages/Login.vue';
-import Register from '@js/pages/Register.vue';
-import RequestResetPassword from '@js/pages/RequestResetPassword.vue';
-import ResetPassword from '@js/pages/ResetPassword.vue';
+const AuthLayout = () => import(/* webpackChunkName: "AuthLayout" */ '@js/layouts/AuthLayout.vue');
+const Login = () => import(/* webpackChunkName: "Login" */ '@js/pages/Login.vue');
+const Register = () => import(/* webpackChunkName: "Register" */ '@js/pages/Register.vue');
+const RequestResetPassword = () => import(/* webpackChunkName: "RequestResetPassword" */ '@js/pages/RequestResetPassword.vue');
+const ResetPassword = () => import(/* webpackChunkName: "ResetPassword" */ '@js/pages/ResetPassword.vue');
 
 Vue.use(Router);
 
 let router = new Router({
-    routes: [
-        {
+    routes: [{
+        path: '/',
+        name: 'auth',
+        component: AuthLayout,
+        meta: {
+            guest: true
+        },
+        children: [{
             path: '/',
-            name: 'auth',
-            component: AuthLayout,
-            meta: {
-                guest: true
-            },
-            children: [
-                {
-                    path: '/',
-                    redirect: { name: 'login' }
-                },
-                {
-                    path: '/login',
-                    name: 'login',
-                    component: Login,
-                    meta: {
-                        guest: true
-                    }
-                },
-                {
-                    path: '/register',
-                    name: 'register',
-                    component: Register,
-                    meta: {
-                        guest: true
-                    }
-                },
-                {
-                    path: '/request-password-reset',
-                    name: 'request-password-reset',
-                    component: RequestResetPassword,
-                    meta: {
-                        guest: true
-                    }
-                },
-                {
-                    path: '/reset-password',
-                    name: 'reset-password',
-                    component: ResetPassword,
-                    meta: {
-                        guest: true
-                    }
-                },
-            ]
+            redirect: { name: 'login' }
         },
         {
-            path: '/',
-            name: 'dashboard',
-            component: DashBoardLayout,
+            path: '/login',
+            name: 'login',
+            component: Login,
             meta: {
                 guest: true
-            },
-            children: [
-                {
-                    path: '/home',
-                    name: 'home',
-                    component: Home,
-                    meta: {
-                        requiresAuth: true
-                    }
-                },
-                {
-                    path: '/contact',
-                    name: 'contact',
-                    component: Contact,
-                    meta: {
-                        requiresAuth: true
-                    }
-                },
-                {
-                    path: '/about',
-                    name: 'about',
-                    component: About,
-                    meta: {
-                        requiresAuth: true
-                    }
-                }
-            ]
+            }
         },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+            meta: {
+                guest: true
+            }
+        },
+        {
+            path: '/request-password-reset',
+            name: 'request-password-reset',
+            component: RequestResetPassword,
+            meta: {
+                guest: true
+            }
+        },
+        {
+            path: '/reset-password',
+            name: 'reset-password',
+            component: ResetPassword,
+            meta: {
+                guest: true
+            }
+        },
+        ]
+    },
+    {
+        path: '/',
+        name: 'dashboard',
+        component: DashBoardLayout,
+        meta: {
+            guest: true
+        },
+        children: [{
+            path: '/home',
+            name: 'home',
+            component: Home,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: Contact,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About,
+            meta: {
+                requiresAuth: true
+            }
+        }
+        ]
+    },
     ]
 });
 
 router.beforeEach((to, from, next) => {
-    if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
         if (store.getters.getAuth == false) {
             next({
                 name: 'login',
@@ -113,14 +111,13 @@ router.beforeEach((to, from, next) => {
         } else {
             next()
         }
-    } else if(to.matched.some(record => record.meta.guest)) {
-        if(store.getters.getAuth == false){
+    } else if (to.matched.some(record => record.meta.guest)) {
+        if (store.getters.getAuth == false) {
             next()
-        }
-        else{
+        } else {
             next({ name: 'home' })
         }
-    }else {
+    } else {
         next()
     }
 });
