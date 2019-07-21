099367826d64e51902ba053e08a97231b4761732
Cristian David Home <cristian_david033@hotmail.com>
2019-07-21 14:57:04 -0500

    Crear rutas API para los recursos de la App.

--- routes/api.php
+++ routes/api.php
@@ -14,23 +14,50 @@ use Illuminate\Http\Request;
 */
 
 /**
- * Controladores que se encuentran dentro de la carpeta API\Auh
- */
-Route::namespace('API\Auth')->group(function () {
-    Route::get('user', 'LoginController@getCurrentUser')->name('auth.user');
-    Route::post('login', 'LoginController@login')->name('login');
-    Route::post('register', 'RegisterController@register')->name('register');
-    Route::post('logout', 'LoginController@logout')->name('logout');
-    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('requestResetPasswordEmail');
-    Route::post('password/reset', 'ResetPasswordController@reset')->name('resetPassword');
-    Route::post('validate-pwdreset-token', 'ResetPasswordController@checkValidToken')->name('checkPasswordResetToken');
-});
-
-/**
  * @Author: Cristian Home
  * @Date: 2019-06-08 19:49:27
- * @Desc: Creación dínamica de rutas para acceder a los recursos a través de una API.
+ * @Desc: Rutas API de la aplicación.
  */
-Route::apiResources([
-    
-]);
+
+/* Prefijo de nombre para todas las rutas API (api.*) */
+Route::name('api.')->group(function(){
+    /**
+     * Controladores que se encuentran dentro de la carpeta API\Auh
+     */
+    Route::namespace('API\Auth')->group(function () {
+        Route::get('user', 'LoginController@getCurrentUser')->name('auth.user');
+        Route::post('login', 'LoginController@login')->name('login');
+        Route::post('register', 'RegisterController@register')->name('register');
+        Route::post('logout', 'LoginController@logout')->name('logout');
+        Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('requestResetPasswordEmail');
+        Route::post('password/reset', 'ResetPasswordController@reset')->name('resetPassword');
+        Route::post('validate-pwdreset-token', 'ResetPasswordController@checkValidToken')->name('checkPasswordResetToken');
+    });
+    /**
+     * Creación dínamica de rutas para todos los recursos (modelos-controladores)
+     * Cada recurso creara una ruta para cada acción basica de CRUD
+     * y su correspondiente verbo HTTP (POST, GET, PUT, DELETE)
+     */
+    Route::apiResources([
+        'clientes' => 'ClienteController',
+        'proovedores' => 'ProovedorController',
+        'tipos de servicios' => 'TipoServicioController',
+        'tipos de producto' => 'TipoProductoController',
+        'productos' => 'ProductoController',
+        'servicios' => 'ServicioController',
+        'contratos' => 'ContratoController',
+        'garantias' => 'GarantiaController',
+        'dispositivos' => 'DispositivoController',
+        'modelos' => 'ModeloController',
+        'paises' => 'PaisController',
+        'estados' => 'EstadoController',
+        'ciudades' => 'CiudadController',
+        'sedes' => 'SedeController',
+        'estaciones de trabajo' => 'EstacionDeTrabajoController',
+        'cargos' => 'CargoController',
+        'personas' => 'PersonaController',
+        'funcionarios' => 'FuncionarioController',
+        'operadores' => 'OperadorController',
+        'tags' => 'TagController',
+    ]);
+});
