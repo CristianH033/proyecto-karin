f6c14e372768014fdf8cc0832fd3f47e7e5ecace
Cristian David Home <cristian_david033@hotmail.com>
2019-10-17 20:55:45 -0500

    Seeders básicos.

---
+++ database/seeds/FabricantesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FabricantesTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //
+  }
+}
---
+++ database/seeds/TercerosTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TercerosTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //
+  }
+}
