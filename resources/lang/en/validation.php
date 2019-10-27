6b9e015b6e30e75aad1f1ed7b3999a1116c47c62
Cristian David Home <cristian_david033@hotmail.com>
2019-10-26 22:01:03 -0500

    Ajuste configuración Laravel 6

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -11,6 +11,7 @@ return [
     | as the size rules. Feel free to tweak each of these messages here.
     |
     */
+    
   'accepted' => 'The :attribute must be accepted.',
   'active_url' => 'The :attribute is not a valid URL.',
   'after' => 'The :attribute must be a date after :date.',
@@ -97,6 +98,7 @@ return [
   'not_in' => 'The selected :attribute is invalid.',
   'not_regex' => 'The :attribute format is invalid.',
   'numeric' => 'The :attribute must be a number.',
+  'password' => 'The password is incorrect.',
   'present' => 'The :attribute field must be present.',
   'regex' => 'The :attribute format is invalid.',
   'required' => 'The :attribute field is required.',
