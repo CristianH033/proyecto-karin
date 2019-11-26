7ee10353ccc8225b03d9182aa24a81fa4f6e3be9
Cristian David Home <cristian_david033@hotmail.com>
2019-11-26 15:25:21 -0500

    Remover scss-css de Laravel Mix y usar mix.extract()

--- webpack.mix.js
+++ webpack.mix.js
@@ -52,6 +52,4 @@ mix.options({
  */
 mix.webpackConfig({});
 
-mix
-  .js("resources/js/app.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css");
+mix.js("resources/js/app.js", "public/js").extract();
