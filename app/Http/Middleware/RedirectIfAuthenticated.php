da6a924476f249d349328d25316c81dbe7aea52d
Cristian David Home <cristian_david033@hotmail.com>
2019-11-27 03:49:32 -0500

    Modificar middeware de redirección en caso de no autenticación. - No redirige si es un llamado api.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,7 +17,7 @@ class RedirectIfAuthenticated
    */
   public function handle($request, Closure $next, $guard = null)
   {
-    if (Auth::guard($guard)->check()) {
+    if (!$request->expectsJson() && Auth::guard($guard)->check()) {
       return redirect('/home');
     }
 
