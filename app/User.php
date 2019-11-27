e7f9507aaa1e91da9b2d23be4c33621ab8e9eb3a
Cristian David Home <cristian_david033@hotmail.com>
2019-11-27 04:16:53 -0500

    Agregar funciones de OTP a modelo usuario User.

--- app/User.php
+++ app/User.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Cache;
+use App\Notifications\OTPNotification;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -72,5 +75,103 @@ class User extends Authenticatable implements MustVerifyEmail
   public function sendPasswordResetNotification($token)
   {
     $this->notify(new ResetPasswordNotification($token));
+  }
+
+  /**
+   * Obtener el actual código OTP
+   * @return String
+   */
+  public function OTP()
+  {
+    return Cache::get($this->OTPKey());
+  }
+
+  /**
+   * Validar si existe un código OTP vigente
+   * @return Boolean
+   */
+  public function ongoingOTP()
+  {
+    return Cache::has($this->OTPKey());
+  }
+
+  /**
+   * Validar si el OTP ya fue verificado
+   * @return Boolean
+   */
+  public function OTPVerified()
+  {
+    return Cache::get($this->OTPVerifiedKey());
+  }
+
+  /**
+   * Establecer estado de OTP
+   * @param  Boolean  $val
+   */
+  public function SetOTPVerified(bool $val)
+  {
+    Cache::Put(
+      [
+        $this->OTPVerifiedKey() => $val
+      ],
+      Cache::get('time')
+    );
+  }
+
+  /**
+   * Obtener llave de chache OTP
+   * @return String
+   */
+  public function OTPKey()
+  {
+    return "OTP_{$this->id}";
+  }
+
+  /**
+   * Obtener llave de chache OTP Verified
+   * @return String
+   */
+  public function OTPVerifiedKey()
+  {
+    return "OTP_verified_{$this->id}";
+  }
+
+  /**
+   * Obtener llave de chache OTP Time
+   * @return String
+   */
+  public function OTPTimeKey()
+  {
+    return "OTP_time_{$this->id}";
+  }
+
+  /**
+   * Generar y guardar en cache un nuevo código OTP
+   * @return String
+   */
+  public function cacheNewOTP()
+  {
+    $OTP = rand(100000, 999999);
+    $expiration = now()->addMinutes(
+      Config::get('services.2fa.otp.lifetime', 60)
+    );
+    Cache::put(
+      [
+        $this->OTPKey() => $OTP,
+        $this->OTPVerifiedKey() => false,
+        $this->OTPTimeKey() => $expiration
+      ],
+      $expiration
+    );
+    return $OTP;
+  }
+
+  /**
+   * Enviar notificación de código OTP
+   */
+  public function sendOTP()
+  {
+    $OTP = $this->cacheNewOTP();
+    $this->notify(new OTPNotification($OTP));
   }
 }
