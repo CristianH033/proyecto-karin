0ad74810ade1fa4755260a0ff5ff3fae613e33ba
Cristian David Home <cristian_david033@hotmail.com>
2019-11-27 04:54:18 -0500

    Agregar vista 404.

---
+++ resources/js/pages/errors/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="kemble-app" app>
+    <v-content>
+      <v-container fill-height style="height: calc(100vh - 58px);">
+        <v-layout align-center>
+          <v-flex text-center>
+            <h1 class="display-2 primary--text">Error 404</h1>
+            <p>La página que está buscando no existe</p>
+            <v-btn :to="{ name: 'home' }" outlined color="primary">
+              Salir de aquí
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "FourOhFour",
+  props: {},
+  created() {}
+};
+</script>
