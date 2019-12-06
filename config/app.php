f9f7f80b9662d4df838c05f5baa4d3b28c49e23e
Cristian David Home <cristian_david033@hotmail.com>
2019-12-05 22:18:20 -0500

    Registrar service providers laravel scout

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
     /*
      * Package Service Providers...
      */
+    Laravel\Scout\ScoutServiceProvider::class,
+    TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
 
     /*
      * Application Service Providers...
