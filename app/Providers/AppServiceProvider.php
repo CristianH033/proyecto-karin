756c17be311794c78e51eef589274c5198c54927
Cristian David Home <cristian_david033@hotmail.com>
2020-05-20 21:07:49 -0500

    Validadores

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot()
   {
+    // VALIDADORES
+    /**
+     * @Author: Cristian David Home
+     * @Date: 2020-01-23 10:16:40
+     * @Desc: Validar si el campo es único (case insensitive)
+     */
     Validator::extend('iunique', function (
       $attribute,
       $value,
@@ -51,6 +57,33 @@ class AppServiceProvider extends ServiceProvider
         ->count();
     });
 
+    /**
+     * @Author: Cristian David Home
+     * @Date: 2020-01-23 17:21:28
+     * @Desc: Validar si la relación polimorfica existe
+     */
+    Validator::extend('poly_exists', function (
+      $attribute,
+      $value,
+      $parameters,
+      $validator
+    ) {
+      if (!($type = array_get($validator->getData(), $parameters[0], false))) {
+        return false;
+      }
+
+      if (Relation::getMorphedModel($type)) {
+        $type = Relation::getMorphedModel($type);
+      }
+
+      if (!class_exists($type)) {
+        return false;
+      }
+
+      return !empty(resolve($type)->find($value));
+    });
+
+    // REPLACERS DE VALIDADORES
     Validator::replacer('iunique', function (
       $message,
       $attribute,
